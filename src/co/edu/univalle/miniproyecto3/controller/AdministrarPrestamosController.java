81f456776cab6d311b1795924f5ae18dfcf64925
Sebastián Orrego <orrego.sebastian@correounivalle.edu.co>
2023-06-01 16:20:17 -0500

    Accion Eliminar en vista AdministrarPrestamos

--- src/co/edu/univalle/miniproyecto3/controller/AdministrarPrestamosController.java
+++ src/co/edu/univalle/miniproyecto3/controller/AdministrarPrestamosController.java
@@ -81,9 +81,6 @@ public class AdministrarPrestamosController {
         
         administrarPrestamos.getjComboDevolucionUsuarios().setModel(defaultComboBoxUsuario);
         mostrarRecursosDevolucion((int)listaUsuarios.get(0));
-//        if (!listaRecursos.isEmpty()) {
-//            mostrarRecursosDevolucion((int)listaUsuarios.get(0));
-//        }
     }
     
     public void mostrarRecursosDevolucion(Integer index) {;
@@ -119,11 +116,6 @@ public class AdministrarPrestamosController {
     public void cleanFields() {
         defaultComboBoxUsuario.removeAllElements();
         defaultComboBoxRecurso.removeAllElements();
-        
-//        listaUsuarios.clear();
-//        listaPrestamos.clear();
-//        listaRecursos.clear();
-        
         administrarPrestamos.getjComboDevolucionRecursos().setModel(defaultComboBoxRecurso);
         administrarPrestamos.getjComboDevolucionUsuarios().setModel(defaultComboBoxUsuario);
         administrarPrestamos.getjComboPrestamoRecursos().setModel(defaultComboBoxRecurso);
@@ -146,8 +138,8 @@ public class AdministrarPrestamosController {
                 if(administrarPrestamos.getjTabbedPane1().getSelectedIndex() == 0){
                     verificarLlaveDevolucion();
                     prestamoDAO.deletePrestamo(indexPrestamo);
+//                    mostrarItemsDevolucion();
 //                    cleanFields();
-                    mostrarItemsDevolucion();
                 }
                 if(administrarPrestamos.getjTabbedPane1().getSelectedIndex() == 1){
                     int index = administrarPrestamos.getjComboPrestamoRecursos().getSelectedIndex();
@@ -162,9 +154,7 @@ public class AdministrarPrestamosController {
             }
             if (e.getSource() == administrarPrestamos.getjComboDevolucionUsuarios()) {
                 indexU = administrarPrestamos.getjComboDevolucionUsuarios().getSelectedIndex();
-                if (indexU != -1){
-                    mostrarRecursosDevolucion((int)listaUsuarios.get(indexU));
-                }
+                mostrarRecursosDevolucion((int)listaUsuarios.get(indexU));
             }
             if (e.getSource() == administrarPrestamos.getjComboDevolucionRecursos()) {
                 indexR = administrarPrestamos.getjComboDevolucionRecursos().getSelectedIndex();
